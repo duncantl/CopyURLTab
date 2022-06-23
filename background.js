fe43b206f20e2984cd9f5170451922b86955f1c5
Duncan Temple Lang <duncan@r-project.org>
2022-06-22 21:17:53 -0700

    remove console.log

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 function handleKeyCommand(command)
 {
-    console.log("command = " + JSON.stringify(command));
+  //  console.log("command = " + JSON.stringify(command));
 
         // copy the URL of the currently active tab to the clipboard.
     browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT})
