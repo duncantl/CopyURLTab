f9691af106c620b1a3cb4b2e372ff83b5aef422a
Duncan Temple Lang <duncan@r-project.org>
2022-06-22 21:09:16 -0700

    moved from ContextMenu extension

---
+++ background.js
@@ -0,0 +1,12 @@
+function handleKeyCommand(command)
+{
+    console.log("command = " + JSON.stringify(command));
+
+        // copy the URL of the currently active tab to the clipboard.
+    browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT})
+	.then(tabs => browser.tabs.get(tabs[0].id))
+	.then( t => {  navigator.clipboard.writeText( t.url) });
+}
+
+
+browser.commands.onCommand.addListener(handleKeyCommand);
